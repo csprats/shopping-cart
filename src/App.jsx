ecb959edc2ee6705c4d82291d1efe9b0b0f86a10
csprats <222044220+csprats@users.noreply.github.com>
2025-08-18 17:26:03 +0200

    Add the logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const App = () => {
   const [inputValue, setInputValue] = useState('')
+  const [items, setItems] = useState([])
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value)
   }
 
   const handleButtonClick = () => {
+    setItems([...items, inputValue]); 
+
     setInputValue('')
   }
 
+  const deleteItem = (e) => {
+    e.target.parentNode.remove()
+  }
+
+  useEffect(() => {
+    window.scrollTo(0, document.body.scrollHeight)
+  }, [items])
+
   return (
     <>
+      {items.map((item, index) => (
+        <div key={index}>
+          <p>
+            - {item} -
+          </p>
+          <button onClick={deleteItem}>Bought!</button>
+        </div>
+      ))}
       <input type="text" value={inputValue} onChange={handleInputChange}/>
       <br />
       <button onClick={handleButtonClick}>Add to the cart</button>
